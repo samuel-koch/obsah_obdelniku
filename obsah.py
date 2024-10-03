3362647be5634dd8b473becb6f7285fa9962ae5d
sam. <scze103@gmail.com>
2024-10-03 10:21:02 +0200

    Add files via upload

--- obsah.py
+++ obsah.py
@@ -1,10 +1,10 @@
-def obsah_obdelniku()
+def obsah_obdelniku():
     a = int(input("Zadej délku strany a v cm: "))
     b = int(input("Zadej délku strany b v cm: "))
-    if a > 0 and b > 0
+    if a > 0 and b > 0:
         obsah = a * b
         print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
-    else
+    else:
         print(f"Neplatné hodnoty, zkuste znovu")
         obsah_obdelniku()
 
