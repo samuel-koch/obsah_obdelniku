7dc62d977575e2a47b4bf7a1dc54acafbe05dbef
sam. <scze103@gmail.com>
2024-10-03 10:10:36 +0200

    Add files via upload

---
+++ obsah.py
@@ -0,0 +1,7 @@
+def obsah_obdelniku()
+    a = int(input("Zadej délku strany a v cm: "))
+    b = int(input("Zadej délku strany b v cm: "))
+    obsah = a * b
+    print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+
+obsah_obdelniku()
